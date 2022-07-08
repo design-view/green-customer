1f0d7b46d65ffeac6adccc1168d830b80eb7dfb9
pink <pink@naver.com>
2022-07-08 16:40:44 +0900

    배포준비 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import CustomerList from './components/CustomerList';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import DetailCustomer from './components/DetailCustomer';
+import { Route, Routes } from "react-router-dom";
+import CreateCustomer from './components/CreateCustomer';
+import EditCustomer from './components/EditCustomer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<CustomerList />} />
+        <Route path="/detailview/:no" element={<DetailCustomer/>} />
+        <Route path="/editcustomer/:no" element={<EditCustomer/>} />
+        <Route path="/write" element={<CreateCustomer/>} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
