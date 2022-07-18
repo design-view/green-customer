72526f31a5941aba02692df2f8e443bc72f2718a
pink <pink@naver.com>
2022-07-18 11:47:57 +0900

    redux적용

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import DetailCustomer from './components/DetailCustomer';
 import { Route, Routes } from "react-router-dom";
 import CreateCustomer from './components/CreateCustomer';
 import EditCustomer from './components/EditCustomer';
+import CustomerContainer from './components/CustomerContainer';
+import CrateCustomerContainer from './components/CreateCustomerContainer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Routes>
-        <Route path="/" element={<CustomerList />} />
+        <Route path="/" element={<CustomerContainer />} />
         <Route path="/detailview/:no" element={<DetailCustomer/>} />
         <Route path="/editcustomer/:no" element={<EditCustomer/>} />
-        <Route path="/write" element={<CreateCustomer/>} />
+        <Route path="/write" element={<CrateCustomerContainer/>} />
       </Routes>
       <Footer/>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux';
 
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+console.log(store.getState())
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
   <BrowserRouter>
+    <Provider store={store}>
     <App />
+    </Provider>
   </BrowserRouter>
   </React.StrictMode>
 );
